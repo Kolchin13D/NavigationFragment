9111971b390fd8653fc0f6f1ff4048e93c2966ca
Kolchin13D <kolchin13d@gmail.com>
2024-07-10 16:11:45 +0300

    Initial commit

---
+++ app/src/main/java/com/example/navigationfragment/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.navigationfragment;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity
+        implements SecondFragment.OnFragmentInteractionListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    public void OnFragmentInteraction(Uri uri) {
+
+    }
+}
